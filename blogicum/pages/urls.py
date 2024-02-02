c8fce142a11b6e8391bad4d74f0048aba8e62960
Nikolai <kolya.kurenkov.1999@gmail.com>
2024-02-03 01:06:24 +0300

    Fix

--- blogicum/pages/urls.py
+++ blogicum/pages/urls.py
@@ -5,5 +5,5 @@ app_name = 'pages'
 
 urlpatterns = [
     path('about/', views.about, name='about'),
-    path('rules/', views.rules,  name='rules'),
+    path('rules/', views.rules, name='rules'),
 ]
