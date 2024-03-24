13062042420f2612e19bc2a1875df75305a4afb6
Your Name <you@example.com>
2024-03-24 04:10:27 +0300

    f

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -23,9 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-npq609*0r&_xn&5gn87d!qivo)@uzqgxdx(oe-u6gl=z7^g)v4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+]
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
